413921ed33662936649f6ccd023c46daca0f66b7
Ashim khan <116441770+Ashim74@users.noreply.github.com>
2025-08-30 17:43:18 +0530

    Add clipboard history navigation and screens

--- app/src/main/java/com/droidnova/codex_testing/MainActivity.kt
+++ app/src/main/java/com/droidnova/codex_testing/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.droidnova.codex_testing.navigation.AppNavHost
 import com.droidnova.codex_testing.ui.theme.Codex_TestingTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +14,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Codex_TestingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val vm: ClipboardViewModel = viewModel()
+                AppNavHost(viewModel = vm)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Codex_TestingTheme {
-        Greeting("Android")
     }
 }
